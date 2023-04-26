0d4dc3c97b483df12312c162223d97a0ffb267a6
Aymaan Shaikh <34725789+ashaikh23@users.noreply.github.com>
2023-04-26 19:23:53 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,19 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Aymaan Shaikh',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Aymaan Shaikh | Full-Stack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `I eat Potatos`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@ashaikh23`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/ashaikh23`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/aymaan-shaikh/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I fbsdjbajkdjasdbjsbd`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
